27ae7802388ea45d35b2ce583828cb0fc7f4bf0d
jerryenuh <44992605+jerryenuh@users.noreply.github.com>
2019-04-29 03:30:34 -0500

    Create csv.php

---
+++ csv.php
@@ -0,0 +1,54 @@
+<?php  
+$connect = mysqli_connect("localhost", "player", "root", "playerinfo");
+if(isset($_POST["submit"]))
+{
+ if($_FILES['file']['name'])
+ {
+  $filename = explode(".", $_FILES['file']['name']);
+  if($filename[1] == 'csv')
+  {
+   $handle = fopen($_FILES['file']['tmp_name'], "r");
+   while($data = fgetcsv($handle))
+   {
+    $item1 = mysqli_real_escape_string($connect, $data[0]);  
+                $item2 = mysqli_real_escape_string($connect, $data[1]);
+                $item3 = mysqli_real_escape_string($connect, $data[2]);
+                $item4 = mysqli_real_escape_string($connect, $data[3]);
+                $item5 = mysqli_real_escape_string($connect, $data[4]);
+                $item6 = mysqli_real_escape_string($connect, $data[5]);
+                $item7 = mysqli_real_escape_string($connect, $data[6]);
+                $query = "INSERT into esports_players(f_name, l_name, gender, gamer_tag, team_name, proficiency_level, game) values('$item1','$item2','$item3','$item4','$item5','$item6','$item7')";
+                mysqli_query($connect, $query);
+                var_dump($data);
+            print "<pre>";
+            print_r($data);
+            print "</pre>";
+           // $value = "'". implode("','",$row). "'";
+           // echo $value;
+   }
+   fclose($handle);
+   echo "<script>alert('Import done');</script>";
+  }
+ }
+}
+?>  
+
+<!DOCTYPE html>  
+<html>  
+ <head>  
+  <title>Webslesson Tutorial</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>  
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+ </head>  
+ <body>  
+  <h3 align="center">Add Players Using CSV Files</h3><br />
+  <form method="post" enctype="multipart/form-data">
+   <div align="center">  
+    <label>Select CSV File:</label>
+    <input type="file" name="file" />
+    <br />
+    <input type="submit" name="submit" value="Import" class="btn btn-info" />
+   </div>
+  </form>
+ </body>  
+</html>
